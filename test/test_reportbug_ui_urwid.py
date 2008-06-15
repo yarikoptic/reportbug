e7e24dd6ff46bdc4a82a5bace006708afbd00f05
Ben Finney <ben+debian@benfinney.id.au>
2008-06-15 06:18:07 +0000

    Assert modules can be imported without ImportError.

---
+++ test/test_reportbug_ui_urwid.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8; -*-
+
+# test/test_reportbug_ui_urwid.py
+# Part of reportbug, a Debian bug reporting tool.
+#
+# Copyright © 2008 Ben Finney <ben+python@benfinney.id.au>
+# This is free software; you may copy, modify and/or distribute this work
+# under the terms of the GNU General Public License, version 2 or later.
+# No warranty expressed or implied. See the file LICENSE for details.
+
+""" Unit test for reportbug_ui_urwid module
+"""
+
+import scaffold
+
+import reportbug_ui_urwid
