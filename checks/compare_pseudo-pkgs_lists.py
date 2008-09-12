916eef28284283f3755d2191b1fb45397111c1af
Sandro Tosi <matrixhasu@gmail.com>
2008-09-12 05:44:19 +0000

    * checks/compare_pseudo-pkgs_lists.py   - updated location of canonical pseudo-packages list from ftpmaster to     bugs.d.o; thanks to Josip Rodin for the patch; Closes: #498663

--- checks/compare_pseudo-pkgs_lists.py
+++ checks/compare_pseudo-pkgs_lists.py
@@ -3,7 +3,7 @@
 # License: Public domain
 #
 # Python script to compare pseudo-packages listed in reportbug
-# agaists the official list on ftp-master
+# agaists the official list on bugs.debian.org
 
 import sys, os
 sys.path = ['.'] + sys.path
@@ -12,27 +12,27 @@ from reportbug import debianbts
 
 import urllib, re
 
-# separete a sequence of "char not spaces", from at least one space (ftp-master uses tabs), from anything after tabs
+# separete a sequence of "char not spaces", from at least one space (canonical copy uses tabs), from anything after tabs
 # we group the first and the latter, so we get the pseudo-packages name and description
 dictparse = re.compile(r'([^\s]+)\s+(.+)',re.IGNORECASE)
 
-ftpmaster_list = {}
-pseudo = urllib.urlopen('http://ftp-master.debian.org/pseudo-packages.description')
+bdo_list = {}
+pseudo = urllib.urlopen('http://bugs.debian.org/pseudopackages/pseudo-packages.description')
 for l in pseudo:
     m = dictparse.search(l)
-    ftpmaster_list[m.group(1)] = m.group(2)
+    bdo_list[m.group(1)] = m.group(2)
 
 bts_keys=debianbts.debother.keys();
 
-diff_rb_ftp = set(bts_keys)-set(ftpmaster_list)
-diff_ftp_rb = set(ftpmaster_list)-set(bts_keys)
+diff_rb_bdo = set(bts_keys)-set(bdo_list)
+diff_bdo_rb = set(bdo_list)-set(bts_keys)
 
-print "pseudo-pkgs in reportbug not in ftpmaster list:", diff_rb_ftp
+print "pseudo-pkgs in reportbug not in bugs.debian.org list:", diff_rb_bdo
 
-for pkg in diff_rb_ftp:
+for pkg in diff_rb_bdo:
     print "   ", pkg,": ", debianbts.debother[pkg]
 
-print "pseudo-pkgs in ftpmaster list not in reprotbug:", diff_ftp_rb
+print "pseudo-pkgs in bugs.debian.org list not in reportbug:", diff_bdo_rb
 
-for pkg in diff_ftp_rb:
-    print "   ", pkg,": ", ftpmaster_list[pkg]
+for pkg in diff_bdo_rb:
+    print "   ", pkg,": ", bdo_list[pkg]
