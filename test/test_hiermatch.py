ef74a0a72ef57fb8b073b06bada2c4919e00d43e
Sandro Tosi <morph@debian.org>
2009-01-16 09:05:03 +0000

    reportbug/test_hiermatch.py, test/test_hiermatch.py
    * reportbug/test_hiermatch.py, test/test_hiermatch.py
      - removed reportbug/test_hiermatch.py, partially merget into
        test/test_hiermatch.py

--- test/test_hiermatch.py
+++ test/test_hiermatch.py
@@ -12,5 +12,21 @@
 """
 
 import scaffold
+from scaffold import TestCase
 
 from reportbug import hiermatch
+
+test_strings_list = ['Beautiful is better than ugly.',
+                     'Explicit is better than implicit.',
+                     'Simple is better than complex.',
+                     'Complex is better than complicated.']
+
+
+class Test_egrep_list(TestCase):
+    """Test cases for 'egrep_list' """
+
+    def test_it_works(self):
+        """ Should return '4' matches """
+        counts = hiermatch.egrep_list(test_strings_list, 'better')
+        
+        self.failUnlessEqual(len(counts), 4, "Not 4 matches")
