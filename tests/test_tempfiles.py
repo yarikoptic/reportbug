8946d28dfe06c8d2076dd60f51f0f4ef4a78610a
Sandro Tosi <morph@debian.org>
2010-11-12 20:12:07 +0100

    almost complete coverage of reportbug.tempfiles

--- tests/test_tempfiles.py
+++ tests/test_tempfiles.py
@@ -1,6 +1,7 @@
 import unittest2
 
 from reportbug import tempfiles
+import os.path
 
 class TestTempfiles(unittest2.TestCase):
 
@@ -20,3 +21,37 @@ class TestTempfiles(unittest2.TestCase):
         prefix = tempfiles.tempfile_prefix(package='dpkg', extra=extra)
         self.assertIn('dpkg', prefix)
         self.assertIn(extra, prefix)
+
+    def test_open_write_safe(self):
+
+        filename = os.path.dirname(__file__) + '/tempfiletest'
+        # binary file
+        fd = tempfiles.open_write_safe(filename)
+
+        self.assertFalse(fd.closed)
+        fd.close()
+        self.assertTrue(fd.closed)
+
+        # remove temp file
+        tempfiles.cleanup_temp_file(filename)
+
+        filename = os.path.dirname(__file__) + '/tempfiletest'
+        # text file
+        fd = tempfiles.open_write_safe(filename, mode='w')
+
+        self.assertFalse(fd.closed)
+        fd.close()
+        self.assertTrue(fd.closed)
+
+        tempfiles.cleanup_temp_file(filename)
+
+    def test_TempFile(self):
+
+        fd, filename = tempfiles.TempFile()
+
+        self.assertIsNotNone(filename)
+        self.assertFalse(fd.closed)
+        fd.close()
+        self.assertTrue(fd.closed)
+
+        tempfiles.cleanup_temp_file(filename)
